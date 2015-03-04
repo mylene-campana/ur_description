c417d978a404fc982cc3350ba9341e24ed14cf32
Mylene Campana <mylene.campana@laas.fr>
2015-03-04 17:18:44 +0100

    Important cosmetic changes for more clarity and update to viewer

--- scripts/ur5_test.py
+++ scripts/ur5_test.py
@@ -1,28 +1,29 @@
 #/usr/bin/env python
 # Script which goes with ur_description package.
 # Load 6-DoF arm robot to test methods.
-# "roslaunch ur_description ur5.launch"
 
-
-from hpp_ros import ScenePublisher
-from hpp_ros import PathPlayer
 from hpp.corbaserver.ur5_robot import Robot
 from hpp.corbaserver import Client
+from hpp.corbaserver import ProblemSolver
 import time
-import sys
-sys.path.append('/local/mcampana/devel/hpp/src/test-hpp/script')
 
 robot = Robot ('ur5')
 cl = robot.client
-r = ScenePublisher (robot)
+ps =ProblemSolver (robot)
 #robot.setRootJointPosition([0,0,1.2,1,0,0,0]) # only in HPP, not displayed
 
 # q = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'] 6 DoF#
 q1 = [0, -1.57, 1.57, 0, 0, 0]; q2 = [0.2, -1.57, -1.8, 0, 0.8, 0]
 
-cl.problem.setInitialConfig (q1)
-cl.problem.addGoalConfig (q2)
-p = PathPlayer (cl, r)
+ps.setInitialConfig (q1); ps.addGoalConfig (q2)
+
+from hpp.gepetto import Viewer, PathPlayer
+r = Viewer (ps)
+pp = PathPlayer (robot.client, r)
+r.loadObstacleModel ("ur_description","obstacles","obstacles")
+r.loadObstacleModel ("ur_description","table","table")
+r.loadObstacleModel ("ur_description","wall","wall")
+r(q1)
 
 # Load obstacles in HPP #
 cl.obstacle.loadObstacleModel('ur_description','obstacles','') # cylinders
@@ -30,29 +31,26 @@ cl.obstacle.loadObstacleModel('ur_description','table','')
 cl.obstacle.loadObstacleModel('ur_description','wall','') # wall with hole
 
 begin=time.time()
-cl.problem.solve ()
+ps.solve ()
 end=time.time()
 print "Solving time: "+str(end-begin)
+len(ps.getWaypoints (0))
 
-len(cl.problem.nodes ())
-cl.problem.pathLength(0)
+begin=time.time()
+ps.optimizePath (0)
+end=time.time()
+print "Optim time: "+str(end-begin)
+cl.problem.getIterationNumber ()
+ps.pathLength(0)
+ps.pathLength(1)
 
+begin=time.time()
+ps.optimizePath (1)
+end=time.time()
+print "Optim time: "+str(end-begin)
+cl.problem.getIterationNumber ()
+ps.pathLength(2)
 
-# Display obstacles in RViz #
-r.addObject('cylinder1','obstacle_base'); r.addObject('cylinder2','l_object_one')
-r.addObject('table','table_base'); r.addObject('table1','l_foot_one');
-r.addObject('wall1','decor_base'); r.addObject('wall2','l_decor_one');
-r.addObject('wall3','l_decor_two'); r.addObject('wall4','l_decor_three');
-r(q1)
-
-
-# Nodes from the roadmap
-import time
-nodes = cl.problem.nodes ()
-len(nodes)
-for n in  nodes:
-    r (n)
-    time.sleep (.7)
 
 
 ## DEBUG commands
