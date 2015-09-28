dba22810e094d5a12a7cb253e1971c40eaf6dc34
Mylene Campana <mylene.campana@laas.fr>
2015-09-28 15:02:35 +0200

    Use visual meshes for collision, Add sphere obstacles problem

--- scripts/ur5_spheres_test.py
+++ scripts/ur5_spheres_test.py
@@ -10,17 +10,21 @@ robot = Robot ('ur5')
 cl = robot.client
 ps = ProblemSolver (robot)
 
-# q = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'] 6 DoF#
-q1 = [0, -1.57, 1.57, 0, 0, 0]; q2 = [0.2, -1.57, -1.8, 0, 0.8, 0]
-
-ps.setInitialConfig (q1); ps.addGoalConfig (q2)
-
 from hpp.gepetto import Viewer, PathPlayer
 r = Viewer (ps)
 pp = PathPlayer (robot.client, r)
-r.loadObstacleModel ("ur_description","obstacles","obstacles")
+
+# q = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'] 6 DoF#
+q1 = [-0.07, -1.3, 1.4, -0.8, 3.14, 0];
+q2 = [-0.2, -1.65, -1.6, 0.4, 0.8, 0]
+r(q1)
+
+robot.isConfigValid(q1); robot.isConfigValid(q2)
+ps.setInitialConfig (q1); ps.addGoalConfig (q2)
+
+
 r.loadObstacleModel ("ur_description","table","table")
-r.loadObstacleModel ("ur_description","wall","wall")
+r.loadObstacleModel ("ur_description","obstacle_sphere","obstacle_sphere")
 r(q1)
 
 #ps.readRoadmap ('/local/mcampana/devel/hpp/data/ur5-RRT.rdm')
@@ -68,10 +72,6 @@ r.startCapture ("capture","png")
 pp(ps.numberPaths()-1)
 r.stopCapture ()
 
-# Load obstacles in HPP #
-cl.obstacle.loadObstacleModel('ur_description','obstacles','') # cylinders
-cl.obstacle.loadObstacleModel('ur_description','table','')
-cl.obstacle.loadObstacleModel('ur_description','wall','') # wall with hole
 
 ## ffmpeg commands
 ffmpeg -r 50 -i capture_0_%d.png -r 25 -vcodec libx264 video.mp4
